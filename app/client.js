50c8ab8f50a9059a4ce01c2798007d03aff07740
xpeper <xpepermint@gmail.com>
2015-09-12 12:35:19 +0200

    fixing client rendering and history call

--- app/client.js
+++ app/client.js
@@ -3,9 +3,9 @@ import './styles/index.styl';
 import React from 'react';
 import ReactDom from 'react-dom';
 import Router from 'react-router';
-import createHistory from 'history/lib/createHistory'
+import {createHistory} from 'history';
 import routes from './routes';
 
 let history = createHistory();
 
-React.render(<Router history={history}>{routes}</Router>, document.getElementById('app'))
+ReactDom.render(<Router history={history}>{routes}</Router>, document.getElementById('app'))
